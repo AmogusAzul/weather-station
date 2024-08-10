7968eb4e55d5d903624a58d5478242e9efed507d
AmogusAzul <sarfxa0@gmail.com>
2024-08-10 18:15:21 -0500

    use of .env variables for sql

---
+++ server/docker/mysql/tables_init.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+set -e
+
+mysql --protocol=socket -uroot -p$MYSQL_ROOT_PASSWORD <<EOSQL
+
+USE ${MYSQL_DATABASE};
+
+CREATE TABLE station (
+    station_id INT PRIMARY KEY AUTO_INCREMENT,
+    station_owner VARCHAR(255) NOT NULL,
+    latitude DECIMAL(9, 6) NOT NULL CHECK (latitude >= -90 AND latitude <= 90),
+    longitude DECIMAL(10, 6) NOT NULL CHECK (longitude >= -180 AND longitude <= 180)
+);
+
+CREATE TABLE measurement (
+    measurement_id INT PRIMARY KEY AUTO_INCREMENT,
+    random_num INT NOT NULL
+);
+
+CREATE TABLE entry (
+    entry_id INT PRIMARY KEY AUTO_INCREMENT,
+    station_id INT NOT NULL,
+    measurement_id INT NOT NULL,
+    entry_time DATETIME NOT NULL,
+    FOREIGN KEY (station_id) REFERENCES station (station_id),
+    FOREIGN KEY (measurement_id) REFERENCES measurement (measurement_id)
+);
+
+EOSQL
--- server/docker/mysql/user_init.sh
+++ server/docker/mysql/user_init.sh
@@ -1,9 +1,18 @@
-CREATE '${MYSQL_DATABASE}';
+#!/bin/bash
+set -e
 
+mysql --protocol=socket -uroot -p$MYSQL_ROOT_PASSWORD <<EOSQL
+
+-- Create both users for the servers
 CREATE USER '${DB_DATA_SERVER_USER}'@'%' IDENTIFIED WITH 'caching_sha2_password' BY '${DB_DATA_SERVER_PASSWORD}';
 CREATE USER '${DB_USER_SERVER_USER}'@'%' IDENTIFIED WITH 'caching_sha2_password' BY '${DB_USER_SERVER_PASSWORD}';
 
+-- Granting write privileges to the data-server user
 GRANT ALL PRIVILEGES ON ${MYSQL_DATABASE}.* TO '${DB_DATA_SERVER_USER}'@'%';
+
+-- Grant only-read permissions to the user-server user for safety reasons
 GRANT SELECT ON ${MYSQL_DATABASE}.* TO '${DB_USER_SERVER_USER}'@'%';
 
 FLUSH PRIVILEGES;
+
+EOSQL
