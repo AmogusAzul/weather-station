9a4cf54d16e22b243bd32403cf2c63c5e2e53ad3
AmogusAzul <sarfxa0@gmail.com>
2024-08-11 21:57:29 -0500

    tcp receiving

---
+++ server/data-server/tcp/tcp.go
@@ -0,0 +1,60 @@
+package tcp
+
+import (
+	"log"
+	"net"
+	"sync"
+)
+
+type StationListener struct {
+	listener net.Listener
+
+	JobQueue chan net.Conn
+
+	KillChan chan bool
+}
+
+func GetStationLister(port string, jobQueue chan net.Conn) *StationListener {
+
+	listener, err := net.Listen("tcp", ":"+port)
+
+	if err != nil {
+		log.Fatalf("Couldn't start listener error: %v", err)
+	}
+
+	return &StationListener{
+		listener: listener,
+		JobQueue: jobQueue,
+	}
+}
+
+func (sl *StationListener) Listen(wg *sync.WaitGroup) {
+
+	wg.Add(1)
+
+	go func() {
+
+		for {
+
+			conn, err := sl.listener.Accept()
+			if err != nil {
+				break
+
+			}
+			sl.JobQueue <- conn
+		}
+	}()
+
+	go func() {
+
+		<-sl.KillChan
+		sl.listener.Close()
+
+		wg.Done()
+
+	}()
+}
+
+func (sl *StationListener) Close() {
+	sl.KillChan <- true
+}
