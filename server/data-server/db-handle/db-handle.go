32c99a3742944c9af50fa308d736a8f7a661f0fb
AmogusAzul <sarfxa0@gmail.com>
2024-08-20 16:03:04 -0500

    Generic db-handle methods using reflect

--- server/data-server/db-handle/db-handle.go
+++ server/data-server/db-handle/db-handle.go
@@ -3,7 +3,7 @@ package dbhandle
 import (
 	"database/sql"
 	"fmt"
-	"time"
+	"strings"
 )
 
 type DbHandler struct {
@@ -25,64 +25,52 @@ func GetDbHandler(dbUser, dbPassword, dbHost, dbPort, dbName string) (*DbHandler
 	return &DbHandler{
 		db: db,
 	}, err
-
 }
-
 func (dh *DbHandler) Exit() {
 	dh.db.Close()
 }
 
-func (dh *DbHandler) ReadStation(station_id int) (Station, error) {
+func (dh *DbHandler) ReadRowByID(id int, object Table) (Table, error) {
 
-	station := Station{}
-
-	err := dh.db.QueryRow(
-		fmt.Sprintf("SELECT * FROM %s WHERE id = ?",
-			StationTableName),
-		station_id,
-	).Scan(
-		&station.StationID,
-		&station.StationOwner,
-		&station.Latitude,
-		&station.Longitude,
-	)
+	pointers, err := GetPointers(object)
 
 	if err != nil {
-
-		//sql.ErrNoRows
-
-		return station, err
-
+		return object, fmt.Errorf("error getting pointers (%e)", err)
 	}
 
-	return station, nil
+	err = dh.db.QueryRow(
+		fmt.Sprintf("SELECT * FROM %s WHERE %s = ?",
+			object.GetTableName(),
+			object.GetFieldsNames()[0],
+		),
+		id,
+	).Scan(pointers...)
 
+	return object, err
 }
+func (dh *DbHandler) SendRow(object Table) (id int, err error) {
 
-func (dh *DbHandler) SendMeasurement(randomNum int) (Measurement, error) {
+	format := strings.Join(object.GetFieldsNames()[1:], ", ")
+	values := GetValues(object)[1:]
+	placeholders := "?"
+	for i, l := 0, len(values); i < l-1; i++ {
+		placeholders += ", ?"
+	}
 
-	result, err := dh.db.Exec(fmt.Sprintf("INSERT INTO %s (random_num) VALUES (?)", MeasurementTableName), randomNum)
+	result, err := dh.db.Exec(
+		fmt.Sprintf(
+			"INSERT INTO %s (%s) VALUES (%s)",
+			object.GetTableName(),
+			format,
+			placeholders,
+		), values...)
 
 	if err != nil {
-		return Measurement{}, nil
+		return -1, err
 	}
 
-	measurementID, err := result.LastInsertId()
+	id64, err := result.LastInsertId()
+	id = int(id64)
 
-	return Measurement{
-		MeasurementID: int(measurementID),
-		RandomNum:     randomNum,
-	}, err
-
-}
-
-func (dh *DbHandler) SendEntry(measurement Measurement, station Station, time time.Time) (err error) {
-
-	query := fmt.Sprintf(
-		"INSERT INTO %s (station_id, latitude, longitude, measurement_id, entry_time) VALUES (?, ?, ?, ?, ?)",
-		EntryTableName)
-
-	_, err = dh.db.Exec(query, station.StationID, station.Latitude, station.Longitude, measurement.MeasurementID, time)
-
-	return err
+	return
 }
--- server/data-server/db-handle/tables.go
+++ server/data-server/db-handle/tables.go
@@ -1,6 +1,10 @@
 package dbhandle
 
-import "time"
+import (
+	"fmt"
+	"reflect"
+	"time"
+)
 
 const (
 	StationTableName     string = "station"
@@ -8,21 +12,45 @@ const (
 	EntryTableName       string = "entry"
 )
 
+type Table interface {
+	GetTableName() string
+	GetFieldsNames() []string
+}
+
 type Station struct {
-	StationID    int
+	ID           int
 	StationOwner string
 
 	Latitude  float32
 	Longitude float32
 }
 
+func (s Station) GetTableName() string { return StationTableName }
+func (s Station) GetFieldsNames() []string {
+	return []string{
+		"station_id",
+		"station_owner",
+		"latitude",
+		"longitude",
+	}
+}
+
 type Measurement struct {
-	MeasurementID int
+	ID int
 
 	RandomNum int
 }
 
+func (m Measurement) GetTableName() string { return MeasurementTableName }
+func (m Measurement) GetFieldsNames() []string {
+	return []string{
+		"measurement_id",
+		"random_num",
+	}
+}
+
 type Entry struct {
+	ID        int
 	StationID int
 	Latitude  float32
 	Longitude float32
@@ -30,4 +58,54 @@ type Entry struct {
 	MeasurementID int
 
 	EntryTime time.Time
+}
+
+func (e Entry) GetTableName() string { return EntryTableName }
+func (e Entry) GetFieldsNames() []string {
+	return []string{
+		"entry_id",
+		"station_id",
+		"latitude",
+		"longitude",
+		"measurement_id",
+		"entry_time",
+	}
+}
+
+func GetID(object Table) int {
+	val := reflect.ValueOf(object).FieldByName("ID")
+	return int(val.Int())
+}
+func GetValues(object Table) []interface{} {
+	val := reflect.ValueOf(object)
+	values := []interface{}{}
+	for i := 0; i < val.NumField(); i++ {
+		field := val.Field(i)
+		values = append(values, field.Interface())
+	}
+	return values
+}
+func GetPointers(object Table) (pointers []interface{}, err error) {
+	val := reflect.ValueOf(object)
+
+	// Check if the object is a pointer to a struct
+	if valStruct := val.Elem(); val.Kind() != reflect.Ptr ||
+		valStruct.Kind() != reflect.Struct {
+		return pointers, fmt.Errorf("object is meant to be a pointer of a struct")
+	}
+	// Select struct
+	val = val.Elem()
+
+	// Iterate over the fields of the struct
+	for i := 0; i < val.NumField(); i++ {
+		field := val.Field(i)
+		if field.CanAddr() {
+			// Get the pointer to the field and store it in the slice
+			pointers = append(pointers, field.Addr().Interface())
+			continue
+		}
+		err = fmt.Errorf("field called %s isn't addreasable", val.Type().Field(i).Name)
+	}
+
+	return
 }
