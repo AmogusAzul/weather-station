59419bdaed9e1919123b0bfe606bd1b86d55eba4
AmogusAzul <sarfxa0@gmail.com>
2024-10-20 14:34:05 -0500

    moved safety.h to esp8266/safety/

--- common/headers/safety/safety.h
+++
@@ -1,8 +0,0 @@
-#ifndef SAFETY_H
-#define SAFETY_H
-
-#include <cstring>
-
-const size_t TOKEN_LENGTH = 6;
-
-#endif // SAFETY_H
---
+++ station/code/esp8266/safety/safety.h
@@ -0,0 +1,13 @@
+#ifndef SAFETY_H
+#define SAFETY_H
+
+#include <cstring>
+
+namespace safety
+{
+    constexpr uint8_t TOKEN_LENGTH = 6;
+    const uint8_t DEFAULT_TOKEN[] = {0x36, 0x39, 0x36, 0x39, 0x36, 0x39};
+}
+
+
+#endif // SAFETY_H
